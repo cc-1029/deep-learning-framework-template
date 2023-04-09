8856b0b51b31636f461d49844ef11260ed80d7a4
cc-1029 <caichengjie1029@163.com>
2023-04-10 00:06:30 +0800

    new feats

---
+++ base/base_trainer.py
@@ -0,0 +1,27 @@
+import abc
+
+
+class BaseTrainer:
+    def __init__(self, **kwargs):
+        self.set_dict_attrs(kwargs)
+
+    def set_dict_attrs(self, res_dict):
+        for k, v in res_dict.items():
+            setattr(self, k, v)
+
+    def set_attrs_from_config_parser(self, parser):
+        res_dict = {}
+        for k, v in parser.config.items():
+            if k != 'config_args' and k != 'trainer':
+                if type(v) is not list:
+                    res_obj = parser.init_obj(k)
+                else:
+                    res_obj = parser.init_objs(k)
+                res_dict[k] = res_obj
+        for k, v in parser.config_args.__dict__.items():
+            res_dict[k] = v
+        self.set_dict_attrs(res_dict)
+
+    @abc.abstractmethod
+    def train(self):
+        raise NotImplementedError
